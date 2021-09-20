680aece8625d970fac3c37283e0266c5fbbd99cb
Lucifer <63491234+ashish-patwal@users.noreply.github.com>
2021-09-20 13:57:33 +0000

    twilio sucks

--- message.py
+++ message.py
@@ -2,21 +2,19 @@ import os
 from twilio.rest import Client
 
 phno = os.environ['phno']
-
 msg_sid = os.environ['messaging_service_sid']
-
 authtoken = os.environ['authtoken']
-
 acc_sid = os.environ['account_sid']
 
+account_sid = acc_sid
+auth_token = authtoken
+client = Client(account_sid, auth_token)
 
 def sendMessage(message: str):
-    account_sid = acc_sid
-    auth_token = authtoken
-    client = Client(account_sid, auth_token)
-
     message = client.messages.create(
         messaging_service_sid=msg_sid,
         body=message,
         to=phno,
     )
+
+    print(message.sid)
