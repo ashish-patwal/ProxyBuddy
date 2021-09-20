fa15d273827ea0905a0f692d938d317094522e62
Lucifer <63491234+ashish-patwal@users.noreply.github.com>
2021-09-20 14:34:57 +0000

    proxy buddy never dies

--- message.py
+++ message.py
@@ -2,7 +2,7 @@ import os
 from twilio.rest import Client
 
 phno = os.environ['phno']
-msg_sid = os.environ['messaging_service_sid']
+twilioNum = os.environ['twilioNum']
 authtoken = os.environ['authtoken']
 acc_sid = os.environ['account_sid']
 
@@ -12,7 +12,7 @@ client = Client(account_sid, auth_token)
 
 def sendMessage(message: str):
     message = client.messages.create(
-        messaging_service_sid=msg_sid,
+        from_=twilioNum,
         body=message,
         to=phno,
     )
